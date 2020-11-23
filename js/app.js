7df6874aedd26d83c1e8f5f764812579018fd3bf
Radosław Łuckoś <luckos.rr@hotmail.com>
2020-11-23 19:42:51 +0100

    searching for country name added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 const countryElTemp = document.getElementById("countryElTemplate");
 const countriesElList = document.querySelector(".countries_panel");
+const countriesArr = [];
+const searchBtn = document.querySelector(".search-btn");
+
+function createEl(country){
+    const countryEl = document.importNode(countryElTemp.content,true);
+    countryEl.querySelector('h2').textContent = country.name;
+    countryEl.querySelector('img').src = country.flag;
+    countryEl.querySelector('img').alt = `${country.name}'s flag`;
+    countryEl.querySelector('div').classList.add('countryEl');
+    countriesElList.append(countryEl);
+}
+
+function search() {
+    const searchInputValue = document.getElementById("search").value;
+    const filteredCountries = countriesArr.filter((obj)=>{
+        return obj.name.toLowerCase().includes(searchInputValue.toLowerCase());
+    });
+    countriesElList.innerHTML="";
+    for (const country of filteredCountries) {
+        createEl(country);
+    }
+}
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://restcountries.eu/rest/v2/all?fields=name;flag;region");
@@ -7,16 +29,15 @@ xhr.responseType = 'json';
 xhr.onload = ()=>{
     const listOfCountries = xhr.response;
     for(const country of listOfCountries){
-       const countryEl = document.importNode(countryElTemp.content,true);
-       countryEl.querySelector('h2').textContent = country.name;
-       countryEl.querySelector('img').src = country.flag;
-       countryEl.querySelector('img').alt = `${country.name}'s flag`;
-       countryEl.querySelector('div').classList.add('countryEl');
-       countriesElList.append(countryEl);
-    }
+        countriesArr.push(country)
+        createEl(country);
+        }
 }
 xhr.send();
 
+searchBtn.addEventListener('click', ()=>{
+    search();
+});
 
 
 
