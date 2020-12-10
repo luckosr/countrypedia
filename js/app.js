2351045428f9e83379c21000f90168095794fd32
Radosław Łuckoś <luckos.rr@hotmail.com>
2020-12-10 17:30:37 +0100

    started adding filter mechanism

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@ const countryElTemp = document.getElementById("countryElTemplate");
 const countriesElList = document.querySelector(".countries_panel");
 const countriesArr = [];
 const searchBtn = document.querySelector(".search-btn");
+const filterBtn = document.querySelector('.filter_btn');
+const regionsList = document.querySelector('.filter_regions');
+const regions = document.querySelectorAll('.region_name');
+
+
+function showData(country){
+    //funkcja wyświetlająca na ekranie dane klikniętego kraju wysyłając zapytanie z jego nazwą do API
+    console.log(country);
+}
 
 function createEl(country){
     const countryEl = document.importNode(countryElTemp.content,true);
@@ -10,6 +19,10 @@ function createEl(country){
     countryEl.querySelector('img').alt = `${country.name}'s flag`;
     countryEl.querySelector('div').classList.add('countryEl');
     countriesElList.append(countryEl);
+    countryEl.addEventListener('click', ()=>{
+        showData(country);
+        console.log("clicked");
+    },true);
 }
 
 function search() {
@@ -21,6 +34,11 @@ function search() {
     for (const country of filteredCountries) {
         createEl(country);
     }
+}
+
+function filter(region){
+    //funkcja filtrująca wyniki według regionu
+    console.log(region);
 }
 
 const xhr = new XMLHttpRequest();
@@ -35,9 +53,17 @@ xhr.onload = ()=>{
 }
 xhr.send();
 
-searchBtn.addEventListener('click', ()=>{
-    search();
+searchBtn.addEventListener('click', ()=>search());
+regions.forEach(region => {
+    region.addEventListener('click', (e)=>{
+        // console.log(e.target.id);
+        const filter_region = e.target.id;
+        filter(filter_region)
+    })
 });
+filterBtn.addEventListener('click', ()=>{
+    regionsList.classList.toggle('inactive');
+})
 
 
 
